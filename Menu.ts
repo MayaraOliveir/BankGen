3a77e535393fcdb6804d82c5f7cac03d09fecf1a
MayaraOliveir <mayaraoliveir00@gmail.com>
2025-09-02 23:44:23 -0300

    criando conta corrente

--- Menu.ts
+++ Menu.ts
@@ -1,9 +1,22 @@
 import readlinesync = require("readline-sync");
+import { Conta } from "./models/Conta";
 
 
 export function main() {
     
     let opcao: number;
+
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    conta.visualizar();
+    conta.sacar(10500);
+    conta.visualizar();
+    conta.depositar(5000);
+    conta.visualizar();
+
+    console.log("Menu...")
+
+
+
     while (true) {
 
 
--- models/Conta.ts
+++ models/Conta.ts
@@ -64,5 +64,33 @@ export class Conta{
         return false;
     }
 
+    public depositar(valor: number): void{
+        this._saldo = this._saldo + valor;
+    }
+
+    public visualizar(): void{
+        let tipo: string = "";
+
+        switch (this._tipo) {
+            case 1:
+                tipo = "Conta Corrente";
+                break;
+            case 2:
+                tipo = "Conta Poupança";
+                break;
+        }
+
+
+        console.log("\n\n*****************************************************");
+        console.log("Dados da Conta:");
+        console.log("*****************************************************");
+        console.log(`Numero da Conta: ${this._numero}`);
+        console.log("Agência: " + this._agencia);
+        console.log("Tipo da Conta: " + tipo);
+        console.log("Titular: " + this._titular);
+        console.log("Saldo: " + this._saldo.toFixed(2));
+
+    }
+
 }
 
